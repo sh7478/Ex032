3ff33d8545e1af84f7b27bcc2df31b5b698eae86
sh7478 <sh7478@bs.amalnet.k12.il>
2025-11-29 15:25:11 +0200

    added Book

---
+++ app/src/main/java/com/example/ex032/Book.java
@@ -0,0 +1,39 @@
+package com.example.ex032;
+
+public class Book extends LibraryItem implements Searchable, Displayable{
+    private String author;
+    private int numOfPages;
+
+    public Book(int yearPublished, String title, String author, int numOfPages) {
+        super(yearPublished, title);
+        this.author = author;
+        this.numOfPages = numOfPages;
+    }
+
+    @Override
+    public void displayFullDetails() {
+        System.out.println(toString());
+    }
+
+    @Override
+    public double calculateLateFee(int daysPastDue) {
+        return 0.5 * daysPastDue;
+    }
+
+    @Override
+    public boolean matches(String query) {
+        if(this.getTitle().contains(query) || this.author.contains(query))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Book{" +
+                "author='" + author + '\'' +
+                ", numOfPages=" + numOfPages +
+                '}';
+    }
+}
